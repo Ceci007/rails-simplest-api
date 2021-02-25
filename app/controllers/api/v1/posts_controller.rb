c8a165d7db502b35ba85c8d3067ebcda63054200
Ceci007 <cecibenitezca@gmail.com>
2021-02-25 12:32:38 -0300

    Add article migration and posts controller

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,18 @@
+module Api
+  module V1
+    class PostsController < ApplicationController
+      def index 
+      end
+
+      def show 
+      end
+
+      def destroy 
+      end
+
+      private
+      def set_article 
+      end
+    end
+  end
+end
