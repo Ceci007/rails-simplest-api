9365959d992d93a3b9272645551de1e6dd91b0aa
Ceci007 <cecibenitezca@gmail.com>
2021-02-25 16:46:07 -0300

    Added some actions to posts controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,13 @@
 module Api
   module V1
     class PostsController < ApplicationController
+      before_action :set_article, only: [:show, :destroy]
+
       def index 
+        render json: Article.all
+      end
+
+      def create 
       end
 
       def show 
@@ -12,6 +18,11 @@ module Api
 
       private
       def set_article 
+        @article = Article.find(params[:id])
+      end
+
+      def article_params 
+        params.permit(:title)
       end
     end
   end
